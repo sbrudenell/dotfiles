e0318bf79e3cef62dcd3ecc7b3c8f3e1412b407a
Steven Brudenell <brudenel@justin.tv>
2016-08-09 11:19:14 -0700

    Make setup.sh work without 'ln -sr'

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 cd $(dirname "$0")
+relpath=$(python -c "import os; print os.path.relpath('$PWD', '$HOME')")
 
-[ ! -f ~/.vimrc ] && ln -sr vimrc ~/.vimrc
-[ ! -d ~/.vim ] && ln -sr vim ~/.vim
-[ ! -f ~/.screenrc ] && ln -sr screenrc ~/.screenrc
-[ ! -f ~/.astylerc ] && ln -sr astylerc ~/.astylerc
+[ ! -f ~/.vimrc ] && ln -s "$relpath/vimrc" ~/.vimrc
+[ ! -d ~/.vim ] && ln -s "$relpath/vim" ~/.vim
+[ ! -f ~/.screenrc ] && ln -s "$relpath/screenrc" ~/.screenrc
+[ ! -f ~/.astylerc ] && ln -s "$relpath/astylerc" ~/.astylerc
 
 if [ ! -f ~/.bashrc_local ]; then
   [ -f ~/.bashrc ] && mv ~/.bashrc ~/.bashrc_local
   [ -f ~/.bash_profile ] && mv ~/.bash_profile ~/.bash_profile_local
-  ln -sr bash_profile ~/.bash_profile
-  ln -sr bashrc ~/.bashrc
+  ln -s "$relpath/bash_profile" ~/.bash_profile
+  ln -s "$relpath/bashrc" ~/.bashrc
 fi
 
 [ -f /etc/lsb-release ] && source /etc/lsb-release
